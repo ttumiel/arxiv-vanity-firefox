3ff94e9584f3c0a697439003888f903173b6d114
Thomas Tumiel <ttumiel@gmail.com>
2019-09-28 21:57:56 +0200

    adjust so that getting to arxiv.org from vanity is possible

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 browser.webRequest.onBeforeRequest.addListener(
   requestDetails => {
-    return {
-      redirectUrl:
-        "https://www.arxiv-vanity.com/convert/?query=" +
-        encodeURIComponent(requestDetails.url)
-    };
+    if (
+      requestDetails.originUrl !== undefined &&
+      requestDetails.originUrl.startsWith("https://www.arxiv-vanity.com")
+    ) {
+      return;
+    } else {
+      return {
+        redirectUrl:
+          "https://www.arxiv-vanity.com/convert/?query=" +
+          encodeURIComponent(requestDetails.url)
+      };
+    }
   },
   { urls: ["https://arxiv.org/abs/*"] },
   ["blocking"]
